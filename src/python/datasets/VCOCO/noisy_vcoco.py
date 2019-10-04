9360ac8019a983961bd71b1e5b43c90b23c5dcdb
Tengyu Liu <24.jason.5@gmail.com>
2019-10-03 19:10:51 -0700

    noisy vcoco implementation

--- src/python/datasets/VCOCO/noisy_vcoco.py
+++ src/python/datasets/VCOCO/noisy_vcoco.py
@@ -27,6 +27,7 @@ import torchvision
 import torchvision.transforms
 
 import matplotlib.pyplot as plt
+from skimage import io, transform, color
 
 def get_model():
     feature_network = feature_model.Resnet152(num_classes=len(metadata.action_classes))
@@ -50,7 +51,7 @@ def get_model():
     return feature_network, transform
 
 def combine_box(box1, box2):
-    return np.hstack((np.minimum(box1[:2], box2[:2]), np.maximum(box1[2:], box2[2:])))
+    return np.hstack((np.maximum(box1[:2], box2[:2]), np.minimum(box1[2:], box2[2:])))
 
 mean = np.array([0.485, 0.456, 0.406])
 std = np.array([0.229, 0.224, 0.225])
@@ -60,33 +61,32 @@ def img_to_torch(img, t):
     input: H x W x C img iterables with range 0-255
     output: C x H x W img tensor with range 0-1, normalized
     """
+    img = np.array(img) / 255.
+    img = (img - mean) / std
+    if len(img.shape) == 3:
+        img = img.transpose([2,0,1])
+    elif len(img.shape) == 4:
+        img = img.transpose([0,3,1,2])
+    elif len(img.shape) == 5:
+        img = img.transpose([0,1,4,2,3])
+    img = torch.autograd.Variable(torch.Tensor(img)).cuda()
+    return img
 
-    # img = np.array(img) / 255.
-    # img = (img - mean) / std
-    # if len(img.shape) == 3:
-    #     img = img.transpose([2,0,1])
-    # elif len(img.shape) == 4:
-    #     img = img.transpose([0,3,1,2])
-    # elif len(img.shape) == 5:
-    #     img = img.transpose([0,1,4,2,3])
-    # img = torch.autograd.Variable(torch.Tensor(img)).cuda()
-    # return img
-
-    img = np.array(img)
-    if len(img.shape) == 4:
-        new_img = []
-        for _i in img:
-            new_img.append(t(_i))
-        return torch.autograd.Variable(torch.stack(new_img, dim=0)).cuda()
-    if len(img.shape) == 5:
-        new_img = []
-        for _j in img:
-            new_img.append([])
-            for _i in _j:
-                new_img[-1].append(t(_i))
-            new_img[-1] = torch.stack(new_img[-1], dim=0)
-        return torch.autograd.Variable(torch.stack(new_img, dim=0)).cuda()
-
+def transform_bbox(boxes, mat):
+    bboxes2 = np.zeros([boxes.shape[0], 4, 2])
+    bboxes2[:,:2,0] = boxes[:,[2]]
+    bboxes2[:,2:,0] = boxes[:,[0]]
+    bboxes2[:,::2,1] = boxes[:,[3]]
+    bboxes2[:,1::2,1] = boxes[:,[1]]
+    bboxes_transformed = np.zeros(bboxes2.shape)
+    bboxes_transformed[:,:,0] = bboxes2[:,:,0] * mat[0,0] + bboxes2[:,:,1] * mat[0,1] + mat[0,2] # X = a0*x + a1*y + a2
+    bboxes_transformed[:,:,1] = bboxes2[:,:,0] * mat[1,0] + bboxes2[:,:,1] * mat[1,1] + mat[1,2] # Y = b0*x + b1*y + b2
+    boxes = np.zeros([boxes.shape[0], 4])
+    boxes[:,2] = np.min(bboxes_transformed[:,:,0], axis=1)
+    boxes[:,0] = np.max(bboxes_transformed[:,:,0], axis=1)
+    boxes[:,3] = np.min(bboxes_transformed[:,:,1], axis=1)
+    boxes[:,1] = np.max(bboxes_transformed[:,:,1], axis=1)
+    return boxes
 
 class NoisyVCOCO(torch.utils.data.Dataset):
     def __init__(self, root, imageset, node_feature_appd=False):
@@ -111,18 +111,21 @@ class NoisyVCOCO(torch.utils.data.Dataset):
         img_name = self.coco.loadImgs(ids=[self.unique_image_ids[index]])[0]['file_name']
         img_type = img_name.split('_')[1]
         try:
-            img = cv2.imread(os.path.join(os.path.dirname(__file__), '../../../../', 'data/COCO_train2014_000000000368.jpg'), cv2.IMREAD_COLOR)
+            # img_path = os.path.join(vcoco_path, 'coco/coco/images', '{}2014'.format(img_type), img_name)
+            # img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+            img = io.imread('/home/tengyu/Documents/github/Part-GPNN/data/COCO_train2014_000000000368.jpg')
             # data = pickle.load(open(os.path.join(self.root, '..', 'processed', 'resnet', '{}.p'.format(img_name)), 'rb'))
             # _edge_features = np.load(os.path.join(self.root, '..', 'processed', 'resnet', '{}_edge_features.npy').format(img_name))
             # _node_features = np.load(os.path.join(self.root, '..', 'processed', 'resnet', '{}_node_features.npy').format(img_name))
             data = pickle.load(open(os.path.join(os.path.dirname(__file__), '../../../../', 'data/feature_resnet_noisy', '{}.p'.format(img_name)), 'rb'))
-            _edge_features = np.load(os.path.join(os.path.dirname(__file__), '../../../../', 'data/feature_resnet_noisy', '{}_edge_features.npy').format(img_name))
-            _node_features = np.load(os.path.join(os.path.dirname(__file__), '../../../../', 'data/feature_resnet_noisy', '{}_node_features.npy').format(img_name))
+            # _edge_features = np.load(os.path.join(os.path.dirname(__file__), '../../../../', 'data/feature_resnet_noisy', '{}_edge_features.npy').format(img_name))
+            # _node_features = np.load(os.path.join(os.path.dirname(__file__), '../../../../', 'data/feature_resnet_noisy', '{}_node_features.npy').format(img_name))
         except IOError:
             raise
             warnings.warn('data missing for {}'.format(img_name))
             return self.__getitem__(3)
 
+        h, w, _ = img.shape
         img_id = data['img_id']
         adj_mat = data['adj_mat']
         node_labels = data['node_labels']
@@ -144,15 +147,63 @@ class NoisyVCOCO(torch.utils.data.Dataset):
         node_features = np.zeros([part_num + obj_num, 2000])
         edge_features = np.zeros([part_num + obj_num, part_num + obj_num, 1000])
 
+        # apply transformation
+        if random.random() < 0.2:
+            shear = random.random() * 30 - 15        # -15 deg to +15 deg
+            shear = np.deg2rad(shear)
+        else:
+            shear = 0
+        if random.random() < 0.2:
+            rotate = random.random() * 30 - 15       # -15 deg to +15 deg
+            rotate = np.deg2rad(rotate)
+        else:
+            rotate = 0
+        if random.random() < 0.2:
+            scale = [random.random() * 0.3 + 0.85, random.random() * 0.3 + 0.85]     # 0.85 to 1.15
+        else:
+            scale = [1.0, 1.0]
+
+        translation = [img.shape[0] * 0.5, img.shape[1] * 0.5]
+
+        if random.random() < 0.2:
+            # flip
+            img = img[:,::-1,:]
+            for b in part_boxes:
+                b[0] = w - b[0]
+                b[2] = w - b[2]
+            for b in obj_boxes:
+                b[0] = w - b[0]
+                b[2] = w - b[2]
+
+        img = color.rgb2hsv(img)
+        if random.random() < 0.2:
+            # brightness
+            img[:,:,2] += random.random() * 0.3 - 0.15  # -0.15 to + 0.15
+            img = np.clip(img, 0, 1)
+        if random.random() < 0.2:
+            # hue
+            img[:,:,0] += random.random() * 0.5 - 0.25  # -0.15 to + 0.15
+            img = np.clip(img, 0, 1)
+        img = color.hsv2rgb(img)
+
+        transformation = transform.AffineTransform(shear=shear, scale=scale, rotation=rotate, translation=translation)
+        mat = transformation.params
+
+        img = (img * 255).astype(np.uint8)
+        img = transform.warp(img, transformation.inverse, order=0, output_shape=(img.shape[0] * 3, img.shape[1] * 3))
+
+        part_boxes = transform_bbox(part_boxes, mat)
+        obj_boxes = transform_bbox(obj_boxes, mat)
+
         for part_box in part_boxes.astype(int):
-            part_images.append(cv2.resize(img[part_box[1]:part_box[3] + 1, part_box[0]:part_box[2] + 1, :], (input_h, input_w), interpolation=cv2.INTER_LINEAR))
+            part_images.append(cv2.resize(img[part_box[3]:part_box[1] + 1, part_box[2]:part_box[0] + 1, :], (input_h, input_w), interpolation=cv2.INTER_LINEAR))        
         for obj_box in obj_boxes.astype(int):
-            obj_images.append(cv2.resize(img[obj_box[1]:obj_box[3] + 1, obj_box[0]:obj_box[2] + 1, :], (input_h, input_w), interpolation=cv2.INTER_LINEAR))
+            obj_images.append(cv2.resize(img[obj_box[3]:obj_box[1] + 1, obj_box[2]:obj_box[0] + 1, :], (input_h, input_w), interpolation=cv2.INTER_LINEAR))
         for part_box in part_boxes:
             edge_images.append([])
             for obj_box in obj_boxes:
                 edge_box = combine_box(obj_box, part_box).astype(int)
-                edge_images[-1].append(cv2.resize(img[edge_box[1]:edge_box[3] + 1, edge_box[0]:edge_box[2] + 1, :], (input_h, input_w), interpolation=cv2.INTER_LINEAR))
+                edge_images[-1].append(cv2.resize(img[edge_box[3]:edge_box[1] + 1, edge_box[2]:edge_box[0] + 1, :], (input_h, input_w), interpolation=cv2.INTER_LINEAR))
         
         part_images = img_to_torch(part_images, self.transform)
         feat, pred = self.model(part_images)
@@ -168,16 +219,33 @@ class NoisyVCOCO(torch.utils.data.Dataset):
             edge_features[i_part, part_num:, :] = feat.detach().cpu().numpy()
             edge_features[part_num:, i_part, :] = edge_features[i_part, part_num:, :]
 
-        np.save(os.path.join(os.path.dirname(__file__), '../../../../', 'data/feature_resnet_noisy', '{}_node_features2.npy').format(img_name), node_features)
-        np.save(os.path.join(os.path.dirname(__file__), '../../../../', 'data/feature_resnet_noisy', '{}_edge_features2.npy').format(img_name), edge_features)
+        # append bbox and class to node features
+        if self.node_feature_appd:
+            part_eye = np.eye(14)
+            obj_eye = np.eye(81)
 
-        print(np.linalg.norm(node_features - _node_features) / np.linalg.norm(_node_features))
-        print(np.linalg.norm(edge_features - _edge_features) / np.linalg.norm(_node_features))
+            node_features_appd = np.zeros([node_features.shape[0], 6 + 14 + 81])
 
-        # # append bbox and class to node features
-        # if self.node_feature_appd:
-        #     node_features_appd = np.load(os.path.join(self.root, '..', 'processed', 'resnet', '{}_node_features_appd.npy').format(img_name))
-        #     node_features = np.concatenate([node_features, node_features_appd], axis=-1)
+            node_features_appd[:part_num,0] = (part_boxes[:,2] - part_boxes[:,0]) / img_w # relative w
+            node_features_appd[:part_num,1] = (part_boxes[:,3] - part_boxes[:,1]) / img_h # relative h
+            node_features_appd[:part_num,2] = ((part_boxes[:,2] + part_boxes[:,0]) / 2) / img_w # relative cx
+            node_features_appd[:part_num,3] = ((part_boxes[:,3] + part_boxes[:,1]) / 2) / img_h # relative cy
+            node_features_appd[:part_num,4] = (part_boxes[:,2] - part_boxes[:,0]) * (part_boxes[:,3] - part_boxes[:,1]) / (img_w * img_h) # relative area
+            node_features_appd[:part_num,5] = (part_boxes[:,2] - part_boxes[:,0]) / (part_boxes[:,3] - part_boxes[:,1]) # aspect ratio
+            node_features_appd[:part_num,6:6+14] = part_eye[part_classes]
+
+            node_features_appd[part_num:,0] = (obj_boxes[:,2] - obj_boxes[:,0]) / img_w # relative w
+            node_features_appd[part_num:,1] = (obj_boxes[:,3] - obj_boxes[:,1]) / img_h # relative h
+            node_features_appd[part_num:,2] = ((obj_boxes[:,2] + obj_boxes[:,0]) / 2) / img_w # relative cx
+            node_features_appd[part_num:,3] = ((obj_boxes[:,3] + obj_boxes[:,1]) / 2) / img_h # relative cy
+            node_features_appd[part_num:,4] = (obj_boxes[:,2] - obj_boxes[:,0]) * (obj_boxes[:,3] - obj_boxes[:,1]) / (img_w * img_h) # relative area
+            node_features_appd[part_num:,5] = (obj_boxes[:,2] - obj_boxes[:,0]) / (obj_boxes[:,3] - obj_boxes[:,1]) # aspect ratio
+            node_features_appd[part_num:,6+14:] = obj_eye[obj_classes]
+
+            node_features_appd[np.isnan(node_features_appd)] = 0
+            node_features_appd[np.isinf(node_features_appd)] = 0
+
+            node_features = np.concatenate([node_features, node_features_appd], axis=-1)
 
         return edge_features, \
                     node_features, \
@@ -203,11 +271,10 @@ class NoisyVCOCO(torch.utils.data.Dataset):
 
 
 def main(args):
-    start_time = time.time()
-
     subset = ['train', 'val', 'test']
     training_set = NoisyVCOCO(args.data_root, subset[0])
     print('{} instances.'.format(len(training_set)))
+    start_time = time.time()
     edge_features, obj_features, part_features, part_human_id, adj_mat, obj_labels, obj_roles, human_labels, human_roles, obj_boxes, part_boxes, human_boxes, img_id, img_name, human_num, obj_num, obj_classes, part_classes = training_set[113]
     print('Time elapsed: {:.2f}s'.format(time.time() - start_time))
 
