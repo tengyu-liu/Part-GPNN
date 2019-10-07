0707ddf0e869141cc0701b5978bfd195fa3b1671
Tengyu Liu <24.jason.5@gmail.com>
2019-10-07 16:09:57 -0700

    debug

--- src/python/vcoco.py
+++ src/python/vcoco.py
@@ -178,8 +178,11 @@ def loss_fn(pred_adj_mat, adj_mat, pred_node_labels, node_labels, pred_node_role
                 else:
                     j_lower_indices = torch.tensor(j_upper_node - human_nums[batch_i] + part_nums[batch_i]).squeeze()
 
-                print(j_lower_indices, v1.shape)
-                v2 = torch.index_select(v1, dim=1, index=j_lower_indices)
+                try:
+                    v2 = torch.index_select(v1, dim=1, index=j_lower_indices)
+                except:
+                    print(j_upper_node, human_nums[batch_i], obj_nums[batch_i], part_nums[batch_i], j_lower_indices)
+                    raise
                 # pred_adj_mat_lifted[batch_i, i_upper_node, j_upper_node] = torch.max(v2)
                 pred_adj_mat_lifted[batch_i, i_upper_node, j_upper_node] = torch.sum(v2)
             
