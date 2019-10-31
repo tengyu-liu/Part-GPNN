faa764d8bf2c76bbbc3112fced15e8db283bcd45
Tengyu Liu <tengyuliu@ucla.edu>
2019-10-31 10:10:59 -0700

    val

--- src/model/nonstop-validate.py
+++ src/model/nonstop-validate.py
@@ -1,7 +1,7 @@
 import os
 
 name = "new-update-smaller-mat-adaptive-batch-lr1e-5-dropout-0.5"
-epoch = sorted(list(set([int(x[:4]) for x in os.listdir(os.path.dirname(__file__), 'models', name) if x[:4].isnumeric()])))[-1]
+epoch = sorted(list(set([int(x[:4]) for x in os.listdir(os.path.join(os.path.dirname(__file__), 'models', name)) if x[:4].isnumeric()])))[-1]
 
 command = "CUDA_VISIBLE_DEVICES=2 python validate.py --node_num 400 --name %s --restore_epoch %d"%(name, epoch)
 
