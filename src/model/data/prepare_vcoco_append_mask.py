ef5e3e7d6379fbc41da8aa6d4a2928aa7b26ce2e
Tengyu Liu <tengyuliu@ucla.edu>
2019-11-01 12:00:35 -0700

    appd data

--- src/model/data/prepare_vcoco_append_mask.py
+++ src/model/data/prepare_vcoco_append_mask.py
@@ -2,3 +2,25 @@ import os
 import pickle
 import numpy as np
 
+base_dir = os.path.join(os.path.dirname(__file__), '../../../data/feature_resnet_tengyu')
+
+total = len(os.listdir(base_dir))
+count = 0
+
+for fn in os.listdir(base_dir):
+    data = pickle.load(open(os.path.join(base_dir, fn), 'rb'))
+
+    pairwise_action_mask = np.zeros([data['node_num'], data['node_num'], 27])
+    if self.negative_suppression:
+        for i_obj in range(data['part_num'], data['node_num']):
+            pairwise_action_mask[:, i_obj, :] = obj_action_pair[[data['obj_classes'][i_obj - data['part_num']]]]
+            pairwise_action_mask[i_obj, :, :] = obj_action_pair[[data['obj_classes'][i_obj - data['part_num']]]]
+    else:
+        pairwise_action_mask += 1
+
+    data['pairwise_action_mask'] = pairwise_action_mask
+    pickle.dump(open(os.path.join(base_dir, fn), 'wb'), data)
+
+    count += 1
+    print('\r%d/%d'%(count, total), end='', flush=True)
+    
