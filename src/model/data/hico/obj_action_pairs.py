7d8b0dc7824410d3ffc2ff1a01bb67fea973e879
Tengyu Liu <24.jason.5@gmail.com>
2019-11-04 13:44:29 -0800

    obj_action_pair: parallel

--- src/model/data/hico/obj_action_pairs.py
+++ src/model/data/hico/obj_action_pairs.py
@@ -4,13 +4,17 @@ import numpy as np
 
 basedir = os.path.join(os.path.dirname(__file__), '../../../../data/hico/feature')
 
-obj_action_pair = np.zeros([81, 117])
 
 total = len(os.listdir(basedir))
 count = 0
 
 for fn in os.listdir(basedir):
+
+def process(fn, q):
+    basedir = os.path.join(os.path.dirname(__file__), '../../../../data/hico/feature')
     item = pickle.load(open(os.path.join(basedir, fn), 'rb'))
+    obj_action_pair = np.zeros([81, 117])
+
     assert np.sum(item['action_labels'][...,117:]) == 0
 
     for i_node in range(item['part_num'], item['node_num']):
@@ -18,9 +22,23 @@ for fn in os.listdir(basedir):
         obj_cls = item['obj_classes'][i_obj]
         obj_action_pair[obj_cls] += np.sum(item['action_labels'][:, i_node, :117], axis=0)
 
-    count += 1
-    print('\r%d/%d %s'%(count, total, fn), end='', flush=True)
+    return q.put(obj_action_pair)
+
+from multiprocessing as mp
+from queue import Queue
+
+queue = Queue()
+
+p = mp.Pool(32)
+p.map(process, [(x, queue) for x in os.listdir(basedir)])
+
+obj_action_pair = np.zeros([81, 117])
+
+while not queue.empty():
+    obj_action_pair += queue.get()
+    queue.task_done()
 
 obj_action_pair = np.min(obj_action_pair, 1)
 
 pickle.dump(obj_action_pair, open('obj_action_pairs.pkl', 'wb'))
+print(obj_action_pair)
