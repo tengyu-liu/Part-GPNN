97e566326f688d38a09e7b414ba0ace224c67757
Tengyu Liu <24.jason.5@gmail.com>
2019-11-09 21:31:05 -0800

    Update metrics.py

--- src/model/metrics.py
+++ src/model/metrics.py
@@ -96,6 +96,7 @@ def compute_part_mAP(pred, gt, part_classes):
 
     for i_part, i_part_list in enumerate(hake_to_densepose_idx):
         idx = np.in1d(part_classes, i_part_list)
+        print(part_classes, i_part_list, idx)
         pred_sum[i_part] = np.sum(pred[ idx , ...])
         pred_max[i_part] = np.max(pred[ idx , ...])
         pred_mean[i_part] = np.mean(pred[ idx , ...])
