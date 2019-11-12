d7e867a1bf4ea0005be42a2577122826ace8e6e7
Tengyu Liu <24.jason.5@gmail.com>
2019-11-12 15:06:49 -0800

    new obj action pair

--- src/model/data/hico/obj_action_pairs.py
+++ src/model/data/hico/obj_action_pairs.py
@@ -1,55 +1,20 @@
-import os 
-import pickle
+import re
+import os
+
 import numpy as np
 
-basedir = os.path.join(os.path.dirname(__file__), '../../../../data/hico/feature')
+from metadata import hico_classes, action_classes
 
-def process(fns, q):
-    basedir = os.path.join(os.path.dirname(__file__), '../../../../data/hico/feature')
-    
-    total = len(fns)
-    count = 0
+f = open('config/hico_hoi_list.txt')
+lines = list(f.readlines())
+f.close()
 
-    obj_action_pair = np.zeros([81, 117])
+pair = np.zeros([81, 117])
 
-    for fn in fns:
-        item = pickle.load(open(os.path.join(basedir, fn), 'rb'))
+for l in lines:
+    l = re.sub(" +", ' ', l).split(' ')
+    obj_id = hico_classes.index(l[1].strip())
+    action_id = action_classes.index(l[2].strip())
+    pair[obj_id, action_id] = 1
 
-        assert np.sum(item['action_labels'][...,117:]) == 0
-
-        for i_node in range(item['part_num'], item['node_num']):
-            i_obj = i_node - item['part_num']
-            obj_cls = item['obj_classes'][i_obj]
-            obj_action_pair[obj_cls] += np.sum(item['action_labels'][:, i_node, :117], axis=0)
-
-        count += 1
-        print('\r', count, total, fn, end='', flush=True)
-
-    q.put(obj_action_pair)
-
-import threading
-from queue import Queue
-
-queue = Queue()
-all_filenames = os.listdir(basedir)
-filenames = [all_filenames[i::32] for i in range(32)]
-
-ts = []
-for i in range(32):
-    t = threading.Thread(target=process, args=(filenames[i], queue))
-    t.start()
-    ts.append(t)
-
-for i in range(32):
-    ts[i].join()
-
-obj_action_pair = np.zeros([81, 117])
-
-while not queue.empty():
-    obj_action_pair += queue.get()
-    queue.task_done()
-
-obj_action_pair = np.minimum(obj_action_pair, 1)
-
-pickle.dump(obj_action_pair, open('obj_action_pairs.pkl', 'wb'))
-print(obj_action_pair)
+pickle.dump(pair, open('obj_action_pairs.pkl', 'wb'))
