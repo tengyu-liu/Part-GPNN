11a67466285179aa60a6626b7e685915c9e03012
Tengyu Liu <24.jason.5@gmail.com>
2020-01-15 12:06:29 -0800

    debug

--- src/model/data/prepare_vcoco_append_mask.py
+++ src/model/data/prepare_vcoco_append_mask.py
@@ -20,9 +20,9 @@ for fn in os.listdir(base_dir):
     for i_obj in range(data['part_num'], data['node_num']):
         pairwise_action_mask[:, i_obj, :] = obj_action_pair[[data['obj_classes'][i_obj - data['part_num']]]]
         pairwise_action_mask[i_obj, :, :] = obj_action_pair[[data['obj_classes'][i_obj - data['part_num']]]]
-    for i_part in range(part_num):
-        for j_part in range(part_num):
-            if part_human_ids[i_part] == part_human_ids[j_part]:
+    for i_part in range(data['part_num'):
+        for j_part in range(data['part_num'):
+            if data['part_human_id'][i_part] == data['part_human_id'][j_part]:
                 pairwise_action_mask[i_part, j_part, [2,5,16,21,24]] = 1.0
 
     data['pairwise_action_mask'] = pairwise_action_mask
