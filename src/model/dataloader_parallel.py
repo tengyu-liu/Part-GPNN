7af4d8de753ceaf4b2b13a0c58f1250bbae3bede
Tengyu Liu <24.jason.5@gmail.com>
2020-02-18 17:19:38 +0000

    antelope dataloader

--- src/model/dataloader_parallel.py
+++ src/model/dataloader_parallel.py
@@ -176,8 +176,8 @@ class DataLoader:
 
         self.thread = None
 
-        self.coco = vu.load_coco('/home/tengyu/Data/mscoco/v-coco/data')
-        vcoco_all = vu.load_vcoco('vcoco_{}'.format(imageset), '/home/tengyu/Data/mscoco/v-coco/data')
+        self.coco = vu.load_coco('/home/tengyu/dataset/v-coco/data')
+        vcoco_all = vu.load_vcoco('vcoco_{}'.format(imageset), '/home/tengyu/dataset/v-coco/data')
         self.filenames = [os.path.join(self.datadir, x['file_name'] + '.data') for x in self.coco.loadImgs(ids=vcoco_all[0]['image_id'][:, 0].astype(int).tolist()) if os.path.exists(os.path.join(self.datadir, x['file_name'] + '.data'))]
 
         pass
